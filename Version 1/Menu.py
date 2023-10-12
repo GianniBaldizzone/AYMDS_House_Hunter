c720127e257b5c9cb2a5494a5220b5d2870463ce
GianniBaldizzone <gianni.baldizzone@davinci.edu.ar>
2023-10-12 19:48:31 -0300

    Version 1

---
+++ Version 1/Menu.py
@@ -0,0 +1,217 @@
+class Menu:
+#menu principal
+    def mostrar_menu(self):
+      bucle=0
+     
+      while bucle !=1:
+            print(f"=== Menú principal ===")
+            print("Seleccione una opción:")
+            print("1. Reserva")
+            print("2. Hospedaje")
+            print("3. Actividades")
+            print("4. Limpieza")
+            print("5. Salir")
+            print("\n")
+
+            eleccion = input("Ingrese el número de la opción: ")
+            print("\n")
+
+            if eleccion == "1":
+                self.mostrar_reservas()
+            elif eleccion == "2":
+                self.mostrar_hospedaje()
+            elif eleccion == "3":
+                self.mostrar_actividades()
+            elif eleccion == "4":
+                self.mostrar_limpieza()
+            elif eleccion == "5":
+                bucle = 1
+                print("¡Hasta luego!")
+                
+            else:
+                print("Error Opción no válida ---> Por favor seleccione una opción válida")
+#menu principal
+#menu reservas
+    def mostrar_reservas(self):
+      bucle=0
+     
+      while bucle !=1:
+            print(f"=== Menú Reserva ===")
+            print("Seleccione una opción:")
+            print("1. Ver reservas")
+            print("2. Hacer una reserva")
+            print("3. Cancelar reserva")
+            print("4. Modificar reserva")
+            print("5. Volver")
+
+            eleccion = input("Ingrese el número de la opción: ")
+
+            if eleccion == "1":
+                self.ver_reservas()
+            elif eleccion == "2":
+                self.hacer_reserva()
+            elif eleccion == "3":
+                self.modificar_reserva()
+            elif eleccion == "4":
+                self.eliminar_reserva()
+            elif eleccion == "5":
+                print("Volviendo...")
+                self.mostrar_menu()
+            else:
+                print("Error Opción no válida ---> Por favor seleccione una opción válida")
+
+    def ver_reservas(self):
+        print("Mostrando reservas...")
+        print("\n")
+
+    def hacer_reserva(self):
+        print("Haciendo reserva...")
+        print("\n")
+  
+    def modificar_reserva(self):
+        print("Modificando reserva...")
+        print("\n")
+
+    def eliminar_reserva(self):
+        print("Eliminando reserva...")
+        print("\n")
+#menu reservas
+#menu hospedaje
+    def mostrar_hospedaje(self):
+      bucle=0
+     
+      while bucle !=1:
+            print(f"=== Menú Hospedajes ===")
+            print("Seleccione una opción:")
+            print("1. Ver hospedajes")
+            print("2. Hacer nuevo Check_in")
+            print("3. Modificar Check-in")
+            print("4. Eliminar Check-in")
+            print("5. Volver")
+
+            eleccion = input("Ingrese el número de la opción: ")
+
+            if eleccion == "1":
+                self.ver_hospedajes()
+            elif eleccion == "2":
+                self.check_in()
+            elif eleccion == "3":
+                self.modificar_checkin()
+            elif eleccion == "4":
+                self.eliminar_checkin()
+            elif eleccion == "5":
+                print("Volviendo...")
+                print("\n")
+                self.mostrar_menu()
+            else:
+                print("Error Opción no válida ---> Por favor seleccione una opción válida")
+
+    def ver_hospedajes(self):
+        print("Mostrar hospedajes...")
+        print("\n")
+
+    def check_in(self):
+        print("Haciendo el Check-in...")
+        print("\n")
+  
+    def modificar_checkin(self):
+        print("Modificando Check-in...")
+        print("\n")
+
+    def eliminar_checkin(self):
+        print("Eliminando Check-in...")
+        print("\n")
+#menu hospedaje
+#menu actividades
+
+    def mostrar_actividades(self):
+      bucle=0
+     
+      while bucle !=1:
+            print(f"=== Menú Actividades ===")
+            print("Seleccione una opción:")
+            print("1. Ver Actividades disponibles")
+            print("2. Ver reservas")
+            print("3. Reservar actividad")
+            print("4. Modificar reserva")
+            print("5. Eliminar reserva")
+            print("6. Volver")
+
+            eleccion = input("Ingrese el número de la opción: ")
+
+            if eleccion == "1":
+                self.mostrar_actividades()
+            elif eleccion == "2":
+                self.ver_reservas_actividades()
+            elif eleccion == "3":
+                self.hacer_reserva_de_actividades()
+            elif eleccion == "4":
+                self.modificar_reserva_actividades()
+            elif eleccion == "5":
+                self.eliminar_reserva_actividades()
+            elif eleccion == "6":
+                print("Volviendo...")
+                print("\n")
+                self.mostrar_menu()
+            else:
+                print("Error Opción no válida ---> Por favor seleccione una opción válida")
+
+    def mostrar_actividad(self):
+        print("Mostrando actividades disponibles...")
+        print("\n")
+      
+    def ver_reservas_actividades(self):
+        print("Mostrando reservas de actividades...")
+        print("\n")
+    
+    def hacer_reserva_de_actividades(self):
+        print("Realizando reserva de actividades...")
+        print("\n")
+  
+    def modificar_reserva_actividades(self):
+        print("Modificando reserva de actividades...")
+        print("\n")
+
+    def eliminar_reserva_actividades(self):
+        print("Eliminando reserva de actividades...")
+        print("\n")
+#menu actividades
+#menu limpieza
+    def mostrar_limpieza(self):
+      bucle=0
+     
+      while bucle !=1:
+            print(f"=== Menú Limpieza ===")
+            print("Seleccione una opción:")
+            print("1. Ver estado de las habitaciones")
+            print("2. Ver habitaciones disponibles para limpieza")
+            print("3. Modificar estado")
+            print("4. Volver")
+
+            eleccion = input("Ingrese el número de la opción: ")
+
+            if eleccion == "1":
+                self.mostrar_estado_de_habitaciones()
+            elif eleccion == "2":
+                self.mostrar_habitaciones_disponibles_para_limpieza()
+            elif eleccion == "3":
+                self.modificar_estado_de_las_habitaciones()
+            elif eleccion == "4":
+                print("Volviendo...")
+                print("\n")
+                self.mostrar_menu()  
+            else:
+                print("Error Opción no válida ---> Por favor seleccione una opción válida")
+
+    def mostrar_estado_de_habitaciones(self):
+        print("Mostrando estados de las habitaciones...")
+        print("\n")
+      
+    def mostrar_habitaciones_disponibles_para_limpieza(self):
+        print("Mostrando habitaciones disponibles para limpiar...")
+        print("\n")
+    
+    def modificar_estado_de_las_habitaciones(self):
+        print("Modificando estado de la habitación...")
+        print("\n")
+#menu limpieza
